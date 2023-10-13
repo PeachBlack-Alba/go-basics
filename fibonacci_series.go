6316b9b2825658028bb8a56f772f83915accee2e
PeachBlack-Alba <peachblackweb@gmail.com>
2023-10-13 18:01:39 +0200

    fibonacci

--- fibonacci_series.go
+++ fibonacci_series.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// a series of numbers in which each number ( Fibonacci number )
+//is the sum of the two preceding numbers.
+//The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 func fibonacci(n int) int {
 	if n < 2 {
 		return n
